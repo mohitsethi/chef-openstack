6160406adb96170c1d028963444f373a2ecea896
mohitsethi <mohit@sethis.in>
2014-12-26 20:23:03 +0530

    updated ckbk dependency

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      'This is a library cookbook to manage resources on Openstack'
 long_description 'This is a library cookbook to manage resources on Openstack'
 version          '0.1.0'
 
+depends 'build-essential'
+depends 'apt'
+depends 'yum'
